6fab9fb7bd2d75bb8493d532e7fbac89556601cb
Susilane <susi.planos@gmail.com>
2023-05-03 22:46:56 -0300

    feat: add style card

---
+++ src/components/Card/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledCard = styled.li`
+.contanier_user{
+  display: flex;
+  gap: 0.2em;
+  align-items: center;
+  img{
+    width: 40px;
+  }
+}
+  
+`;
