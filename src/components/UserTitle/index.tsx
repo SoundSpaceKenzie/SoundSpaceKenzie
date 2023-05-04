7a753b14652b6ac37e1586b946f832ef0612f7d9
joseernanejob <jose.ernane.job@gmail.com>
2023-05-04 14:29:35 -0300

    feat: add auto login

--- src/components/UserTitle/index.tsx
+++ src/components/UserTitle/index.tsx
@@ -1,13 +1,25 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { Container } from './style';
 import { UserContext } from '../../providers/User/UserContext';
+import { IUser } from '../../providers/User/@types';
 
 export const UserTitle = () => {
-  const { User } = useContext(UserContext);
+  const [userLocal, setUserLocal] = useState<IUser>();
+  const { User, setUser } = useContext(UserContext);
+  const userLog = localStorage.getItem('@SoundSpace:User');
+
+  useEffect(() => {
+    if (userLog) {
+      setUserLocal(JSON.parse(userLog));
+      setUser(JSON.parse(userLog));
+    } else {
+      setUserLocal(User);
+    }
+  }, []);
 
   return (
     <Container>
-      <h2>Olá,{User.name}</h2>
+      <h2>Olá,{userLocal?.name}</h2>
       <p>O que você está escutando hoje?</p>
     </Container>
   );
